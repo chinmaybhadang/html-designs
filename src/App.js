90a06ecd2768d9cd1220c9b770f509a0d2d314e0
Chinmay Bhadang <cbhadang@ipromoteu.com>
2024-01-29 15:39:42 +0530

    image tag

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p className="font-size">LALA CLASS</p>
-        <p id="font-size2">LALA ID</p>
+        <p id="font-size2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident praesentium voluptatibus ducimus nesciunt dolore. Molestiae sint facilis impedit voluptate numquam, corporis itaque aliquam maxime dolor rerum explicabo mollitia ad! Soluta, accusantium? Aspernatur, aliquid. Pariatur minus error similique nam. Tenetur et tempora consequuntur sunt aperiam! Beatae accusamus dolore mollitia officiis ad.</p>
+        <a href="http://google.com" className="font-size" target="_blank" rel="noreferrer">CLICK HERE</a>
       </header>
+    
     </div>
   );
 }
